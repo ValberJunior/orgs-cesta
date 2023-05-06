f6f7d31a684db9b4fe232713b7cf12f42bdf1978
ValberJunior <valberjr1992@gmail.com>
2023-05-06 16:54:22 -0300

    ✨feature: create mock file

---
+++ src/mock/cart.js
@@ -0,0 +1,18 @@
+import Logo from '../../assets/logo.png';
+import TopImg from '../../assets/topo.png';
+
+const cartData = {
+    top:{
+        background: TopImg,
+        title: "Detalhe da Cesta"
+    },
+    details:{
+        name: "Cesta de Verduras",
+        logo: Logo,
+        label: "Jenny Jack Farm",
+        description: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+        price: 40
+    }
+};
+
+export default cartData
