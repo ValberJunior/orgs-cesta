080a9a9fcaa6e40e674f7d121894e9545e805ccb
ValberJunior <valberjr1992@gmail.com>
2023-05-06 15:13:18 -0300

    ✨feature: add google fonts

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View, Text } from 'react-native';
 import { Cesta } from './src/screens';
+//intl
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+//fonts
+import { useFonts,
+         Montserrat_400Regular,
+         Montserrat_700Bold } from '@expo-google-fonts/montserrat'
 
 export default function App() {
+
+  const [ fontLoaded ] = useFonts({
+   "MontserratRegular": Montserrat_400Regular,
+   "MontserratBold": Montserrat_700Bold
+  });
+
+  if(!fontLoaded){
+    return <View>
+      {/* Podemos colocar uma animacao ou um skeleton */}
+      <Text>Carregando ...</Text>
+    </View>
+  }
+
   return <SafeAreaView>
   <StatusBar/>
   <Cesta/>
