ae6342cfef8391fd7b0d0b597bec263364a8f594
ValberJunior <valberjr1992@gmail.com>
2023-05-06 17:30:41 -0300

    ✨feature: update mock file

--- src/mock/cartData.js
+++ src/mock/cartData.js
@@ -1,17 +1,47 @@
 import Logo from '../../assets/logo.png';
 import TopImg from '../../assets/topo.png';
+import tomate from '../../assets/fruits/Tomate.png';
+import brocolis from '../../assets/fruits/Brócolis.png';
+import batata from '../../assets/fruits/Batata.png';
+import pepino from '../../assets/fruits/Pepino.png';
+import abobora from '../../assets/fruits/Abóbora.png';
 
 const cartData = {
-    top:{
+    top: {
         background: TopImg,
         title: "Detalhe da Cesta"
     },
-    details:{
+    details: {
         name: "Cesta de Verduras",
         logo: Logo,
         label: "Jenny Jack Farm",
         description: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
         price: 40
+    },
+    items: {
+        title: "Itens da cesta",
+        list: [
+            {
+                nome: "Tomate",
+                imagem: tomate,
+            },
+            {
+                nome: "Brócolis",
+                imagem: brocolis,
+            },
+            {
+                nome: "Batata",
+                imagem: batata,
+            },
+            {
+                nome: "Pepino",
+                imagem: pepino,
+            },
+            {
+                nome: "Abóbora",
+                imagem: abobora,
+            }
+        ]
     }
 };
 
