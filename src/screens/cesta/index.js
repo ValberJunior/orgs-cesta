f28b6e1b6925ef569360fc339c8d0269e6519f1b
ValberJunior <valberjr1992@gmail.com>
2023-05-06 15:13:28 -0300

    ✨feature: add content

--- src/screens/cesta/index.js
+++ src/screens/cesta/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import Topo from '../../../assets/topo.png';
+import Logo from '../../../assets/logo.png';
 import styles from './styles';
 
 const Cesta = () => {
@@ -14,6 +15,25 @@ const Cesta = () => {
           style={styles.title}>
             Detalhe da Cesta
         </Text>
+        <View style={styles.wrapper}>
+          <Text style={styles.name}>Cesta de Verduras</Text>
+          <View style={styles.header}>
+            <Image source={Logo} style={styles.logo} />
+            <Text style={styles.label}>Jenny Jack Farm</Text>
+          </View>
+          <Text style={styles.description}>
+            Uma cesta com produtos selecionados 
+            cuidadosamente da fazenda direto 
+            para sua cozinha</Text>
+          <Text style={styles.price}>
+            {
+              Intl.NumberFormat('ptbr',{
+                style: 'currency',
+                currency: 'BRL',
+              }).format(40)
+            }
+          </Text>
+        </View>
     </>
   )
 }
--- src/screens/cesta/styles.js
+++ src/screens/cesta/styles.js
@@ -17,5 +17,42 @@ export default StyleSheet.create({
         fontWeight: 'bold',
         color: '#fff',
         padding: 16
+    },
+    wrapper:{
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    name:{
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontFamily: 'MontserratBold'
+    },
+    header:{
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+    logo:{
+        width: 32,
+        height: 32
+    },
+    label:{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: 'MontserratRegular'
+    },
+    description:{
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'MontserratRegular'
+    },
+    price:{
+        color: '#2A9F85',
+        fontFamily: 'MontserratBold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop:8
     }
 });
